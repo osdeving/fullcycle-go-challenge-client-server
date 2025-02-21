c8b55b5e9dc75246640eb8adee12523c540b4739
Willams Sousa <osdeving@gmail.com>
2025-02-21 13:45:29 -0300

    feat: criar servidor

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,7 +1,14 @@
 package server
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func Start() {
     fmt.Println("Server is running...")
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, World!")
+    })
+    http.ListenAndServe(":8080", nil)
 }
